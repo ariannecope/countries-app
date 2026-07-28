9357d65206cc598a6ba2ab6d8d4cfbab271f310a
ariannecope <arianncope@gmail.com>
2026-07-28 12:05:06 -0500

    Move database credentials to environment variables

--- version-4/server/config.js
+++ version-4/server/config.js
@@ -1,6 +1,7 @@
+import 'dotenv/config';
+
 const config = {
-  databaseUrl:
-    "postgresql://neondb_owner:npg_u6SykTCDfJB9@ep-lingering-night-aj2s67qo-pooler.c-3.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+  databaseUrl: `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}?sslmode=require`,
 };
 
 export default config;
