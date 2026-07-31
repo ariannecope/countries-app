f72571bd2e015ac09706736453ee527c1ed09afb
ariannecope <arianncope@gmail.com>
2026-07-31 12:32:43 -0500

    restore the proxy configuration

--- version-4/client/vite.config.js
+++ version-4/client/vite.config.js
@@ -1,18 +1,18 @@
-// vite.config.js
- import { defineConfig } from "vite";
- import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
- // <https://vite.dev/config/>
- export default defineConfig({
-   server: {
-     proxy: {
-       "/api": {
-        // Proxy API requests from React to our local Express server
-         target: "https://countries-app-znhd.onrender.com",
-         changeOrigin: true,
-         secure: false,
-       },
-     },
-   },
-   plugins: [react()],
- });
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        // Proxy API requests from React to our Express server
+        target: "https://countries-app-znhd.onrender.com",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  plugins: [react()],
+});
